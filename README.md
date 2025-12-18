03f4686e09b90ddd633ceb2a58cf8ae5ed8d6731
almunircomputers9-afk <almunircomputers9@gmail.com>
2025-12-18 21:17:29 +0100

    Add files via upload

--- README.md
+++
@@ -1,2 +0,0 @@
-# AHMAD-MUNIR-MUSA
-i want to create a web for selling cars which has a name SARKIN MOTA AUTO MOBILES 
---
+++ js app.js
@@ -0,0 +1,4 @@
+// Simple example: Alert on contact section click
+document.getElementById('contact').addEventListener('click', function() {
+    alert('Thanks for reaching out! We\'ll get back to you soon.');
+});
